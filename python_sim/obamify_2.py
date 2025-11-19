f970ffed1b633c611defac4e3302fa34bc7ffde7
Po-Chun Wu <pcwu2022@gmail.com>
2025-11-19 13:19:57 +0800

    2.0 OMBFY

--- python_sim/obamify_2.py
+++ python_sim/obamify_2.py
@@ -8,26 +8,26 @@ N = 1000
 M = 1000
 W = 100
 H = 100
-MAX_LENGTH = 5
+MAX_LENGTH = 3
 
 INPUT_SOURCE = './input/bob.png'
-INPUT_TARGETS = ['./input/bill.png']
+INPUT_TARGETS = ['./input/bill.png', './input/obama.png']
 OUTPUT_FILE = './output/obamified.png'
 
 img_source = Image.open(INPUT_SOURCE).resize((W, H))
-img_target = Image.open(INPUT_TARGETS[0]).resize((W, H))
+img_targets = [Image.open(target).resize((W, H)) for target in INPUT_TARGETS]
 source = asarray(img_source).copy()
-target = asarray(img_target).copy()
+targets = [asarray(target).copy() for target in img_targets]
 
 # Compute mean and std for normalization
 source_mean = source.mean(axis=(0, 1))
 source_std = source.std(axis=(0, 1))
-target_mean = target.mean(axis=(0, 1))
-target_std = target.std(axis=(0, 1))
+targets_mean = [target.mean(axis=(0, 1)) for target in targets]
+targets_std = [target.std(axis=(0, 1)) for target in targets]
 
 # Normalize source and target
 source = (source - source_mean) / (source_std + 1e-8)
-target = (target - target_mean) / (target_std + 1e-8)
+targets = [(targets[i] - targets_mean[i]) / (targets_std[i] + 1e-8) for i in range(len(targets))]
 
 def reconstruct(normalized_array, mean, std):
     return (normalized_array * std + mean).astype('uint8')
@@ -80,24 +80,49 @@ total_loss = 0
 # Replace saving normalized float array (which causes KeyError) with a denormalized uint8 image
 obamified_image = Image.fromarray(reconstruct(source, source_mean, source_std))
 obamified_image.save(OUTPUT_FILE)
+
+bagging_results = []
 for epoch in range(N):
-    print(f"Epoch {epoch}: loss = {total_loss}")
     sum_loss = 0
 
+    bagging = [0] * len(targets)
+    max_index = 0
+    if epoch != 0: max_index = bagging_results[-1].index(max(bagging_results[-1]))
     for m in range(M):
         i = random.randint(0, W - 1)
         j = random.randint(0, H - 1)
-        di, dj, loss = pixel_best_move(source, target, i, j)
-        k = i + di
-        l = j + dj
+        dis = [0] * len(targets)
+        djs = [0] * len(targets)
+        loss = -1
+        x_min = 0
+        for x in range(len(targets)):
+            target = targets[x]
+            dis[x], djs[x], loss_x = pixel_best_move(source, target, i, j)
+            if loss == -1 or loss < loss_x: 
+                loss = loss_x
+                x_min = x
+        bagging[x_min] += 1 / M
+        if epoch == 0: max_index = bagging.index(max(bagging))
+        k = i + dis[max_index]
+        l = j + djs[max_index]
         sum_loss += loss
         temp_val_ij = source[i, j].copy() 
         temp_val_kl = source[k, l].copy()
         source[i, j] = temp_val_kl
         source[k, l] = temp_val_ij
-            
+    
+    bagging_results.append(bagging)
+    print(f"Epoch {epoch}: loss = {total_loss}; Bagging: {bagging}")
+         
     obamified_image = Image.fromarray(reconstruct(source, source_mean, source_std))
     obamified_image.save(OUTPUT_FILE)
+
+    plt.figure()
+    plt.plot(bagging_results)
+    plt.xlabel('Epoch')
+    plt.ylabel('Similarity')
+    plt.legend(INPUT_TARGETS)
+    plt.savefig('./output/bagging_results.png')
 
     if sum_loss == 0: break
     total_loss += sum_loss / M
